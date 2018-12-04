988094b91231b52d51544ca2c51c0cb89ef0aa63
Ludovic Dordet <ludovic@dordet.fr>
2018-12-04 12:25:07 +0100

    dev: Add source directory so that dependency sources are automatically found.

---
+++ source/unused.d
@@ -0,0 +1,21 @@
+// Written in the D programming language.
+
+/**
+Unused module.
+
+This file lets editors find automatically dependency source files
+located in the "source" subdirectory.
+
+Copyright: Copyright Ludovic Dordet 2018.
+License:   $(HTTP www.gnu.org/licenses/gpl-3.0.md,
+             GNU GENERAL PUBLIC LICENSE 3.0).
+Authors:   Ludovic Dordet
+*/
+/*
+         Copyright Ludovic Dordet 2018.
+Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+   (See accompanying file LICENSE.md or copy at
+         http://www.gnu.org/licenses/gpl-3.0.md)
+*/
+module unused;
+
